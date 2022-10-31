5d5d9e77ec4e7dd7e43e9d7664e9af3658a65e5a
Sujit Dahal <prosujit@gmail.com>
2022-10-31 22:46:49 +0545

    scripts updated

--- scripts.js
+++ scripts.js
@@ -4,5 +4,4 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
   toggleButton.classList.toggle('active')
-
 })
